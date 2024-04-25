ecc9b087258277ca4fde10c35890a4eb99aeabab
JoseQA81 <161597752+JoseQA81@users.noreply.github.com>
2024-04-25 21:26:36 +0200

    e2 guardar informacion

---
+++ js/e2-guardar-informacion.js
@@ -0,0 +1,6 @@
+//1. DECLARAR UNA VARIABLE:
+//Nombre de variable recomendado en minúscula
+let nombre;
+//Nombre de variable si tiene espacio, usar camelCase, que es la primera palabra minúscula y la siguiente en mayúscula
+let miNombre;
+//Podéis usar guiones para separar los espacios pero es más recomendable camelCase
